4fa6ce12434bd9f77f4232e1ff1b9adeaf4e2728
Blaise  Amisi <blaisemulima21@gmail.com>
2026-04-20 05:10:50 -0600

    Implement BFS algorithm in bfs.py

---
+++ bfs.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph[node]:
+                queue.append(neighbor)
+
+    return visited
